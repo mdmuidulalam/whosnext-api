9f795badf61d6d23ec79f5a73317e0d86d7ccbcb
Md. Muidul Alam <2hin2222@gmail.com>
2019-03-23 18:54:39 +0600

    Take email and password from query string

--- config.js
+++ config.js
@@ -2,20 +2,20 @@ var config = {};
 
 ///Database Seetings
 config.database = {};
-config.database.host = 'localhost';
+config.database.host = "remotemysql.com";
 config.database.port = 3306;
-config.database.name = 'whosnextdb';
-config.database.user = 'sa';
-config.database.password = '12345';
-config.database.dialect = 'mysql';
+config.database.name = "DapPOhT82m";
+config.database.user = "DapPOhT82m";
+config.database.password = "qYD00SyPhF";
+config.database.dialect = "mysql";
 
 ///Password Salt Code
 config.password = {};
 config.password.salt = 12;
 
-/// JWT JWT_SECRET 
-config.jwt = {}
-config.jwt.secret = 'axq6f51'
-config.jwt.maxAge = 3600
+/// JWT JWT_SECRET
+config.jwt = {};
+config.jwt.secret = "axq6f51";
+config.jwt.maxAge = 3600;
 
 module.exports = config;
